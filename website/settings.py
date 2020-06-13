e9ac857c47b3c3f450a514aaa06b7ada2ab5fa1e
Rupesh Solanki <rupeshsolanki78@gmail.com>
2020-06-13 17:38:55 +0530

    update settings.py2

--- website/settings.py
+++ website/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join.(base_dir, 'static/')
+STATICFILES_DIRS = (     
+os.path.join(BASE_DIR, 'static'), 
+) 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
