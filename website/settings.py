172378f3eb7df8507166f561134ab8ef6df0e0c4
root <rupeshsolanki78@gmail.com>
2020-06-15 15:45:37 +0000

    settings

--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'xux@a##4b4@y4h%5c&ag%i@h1o0@%(^4u0k82!1)!pke0)%7=)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
